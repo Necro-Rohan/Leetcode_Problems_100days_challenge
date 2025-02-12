339c287aca709fd38fcbdddf3ef548a847aae502
Rohan Kumar <rohangupta5671@gmail.com>
2025-02-12 21:23:45 +0530

    Time: 31 ms (92.88%), Space: 17.9 MB (22.63%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -2,7 +2,7 @@ class Solution:
     def fib(self, n: int) -> int:
         a = 0
         b = 1
-        for i in range(n):
+        for _ in range(n):
             a,b = b,(a+b)
         return a
         
