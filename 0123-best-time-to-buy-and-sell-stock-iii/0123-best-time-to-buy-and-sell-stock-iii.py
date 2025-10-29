143515c3c625b1ed9c159e388432bd3a83fe2c0a
Rohan Kumar <rohangupta5671@gmail.com>
2025-10-29 16:17:16 +0530

    Time: 182 ms (97.39%), Space: 32.4 MB (39.58%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,71 +1,29 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # n = len(prices)
-        # minn = float('inf')
-        # maxx = 0
-        # sell = []
-        # i = 0
-        # while i < n:
-        #     if prices[i] < minn:
-        #         minn = prices[i]
-        #         maxx = 0
-
-        #     maxx = prices[i] - minn
-        #     sell.append(maxx)
-        #     i+=1
-        
-        # buy = []
-        # j = n - 1
-        # maxx = prices[-1]
-        # minn = 0
-        # while j >= 0:
-        #     if prices[j] > maxx:
-        #         maxx = prices[j]
-        #         minn = 0
-        #     minn = maxx - prices[j]
-        #     buy.append(minn)
-        #     j-=1
-
-        
-        # buy.reverse()
-        # max_idx = 0
-        # for i in range(n):
-        #     if buy[i] >= buy[max_idx]:
-        #         max_idx = i
-        # max_pro = 0
-        # for i in range(max_idx+1):
-        #     max_pro = max(max_pro, sell[i])
+        n = len(prices)
+        buy_at = float('inf')
+        sell_profit = []
+        profit = 0
+        for i in range(n):
+            if prices[i] < buy_at:
+                buy_at = prices[i]
+            profit = max(profit, prices[i] - buy_at)
+            sell_profit.append(profit)
         
 
-        # return max_pro + buy[max_idx]
-        mini = prices[0]
-        sell = []
-        for i in range(len(prices)):
-            if mini > prices[i]:
-                mini = prices[i]
-            sell.append(prices[i] - mini)
-        # print(sell)
-
-        buy = []
-        maxi = prices[-1]
-
-        for j in range(len(prices)-1,-1,-1):
-            if prices[j]> maxi:
-                maxi = prices[j]
-            buy.append(maxi-prices[j])
-        buy = buy[::-1]
+        sell_at = float('-inf')
+        buy_profit = [0]*n
+        for j in range(n-1, -1, -1):
+            if prices[j] > sell_at:
+                sell_at = prices[j]
+            buy_profit[j] = (sell_at - prices[j])
         
-        prefix_max =[]
-        max_2 = 0
-        for k in range(len(sell)):
-            if max_2 < sell[k]:
-                max_2 =sell[k]
-            prefix_max.append(max_2)
-
-        final_ans = 0
-        for t in range(len(prefix_max)):
-            if buy[t] + prefix_max[t] > final_ans:
-                final_ans = buy[t] + prefix_max[t]
+        max_profit = 0
+        for i in range(n):
+            max_profit = max((buy_profit[i] + sell_profit[i]), max_profit)
+        return max_profit
 
 
-        return final_ans
+
+            
+        
